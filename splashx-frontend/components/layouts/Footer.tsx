f479607c7c9238296c2a04f79af52fed22d1faa4
joyjsmun <joy.js.mun@gmail.com>
2023-05-11 03:09:21 +0900

    ADD: footer basic

--- splashx-frontend/components/layouts/Footer.tsx
+++ splashx-frontend/components/layouts/Footer.tsx
@@ -1,5 +1,29 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Image,
+  Link,
+  Text,
+  Flex,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { ReactNode } from "react";
 
 export default function Footer() {
-  return <Box>Footer</Box>;
+  return (
+    <Box
+      bg={useColorModeValue("black", "gray.900")}
+      color={useColorModeValue("brand.gray", "gray.200")}
+    >
+      <Box py={4}>
+        <Flex justifyContent={"center"} gap={2}>
+          <Link>Twitter</Link>
+          <Text>|</Text>
+          <Link>Telegram</Link>
+        </Flex>
+        <Text pt={2} fontSize={"sm"} textAlign={"center"}>
+          ⓒ 2023 SplashX. All rights reserved.
+        </Text>
+      </Box>
+    </Box>
+  );
 }
