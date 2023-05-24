7aef959799d46fb9e202b0c1eb976a9fda738736
joymun <joy.js.mun@gmail.com>
2023-05-24 23:53:35 +0900

    ADD: grid css changes trending animes

--- splashx-frontend/components/TreandingAnime.tsx
+++ splashx-frontend/components/TreandingAnime.tsx
@@ -10,22 +10,22 @@ import {
 
 export default function TrendingAnime() {
   return (
-    <Box>
+    <Box mb={"3"}>
       <VStack alignItems={"flex-start"} pt={"20"}>
         <Text fontSize={"4xl"} fontWeight={"bold"} pb={4}>
           Trending Anime
         </Text>
         <Grid templateColumns="repeat(4, 1fr)" gap={6}>
-          <GridItem w="100%" h="10">
+          <GridItem w="100%" h="max-content">
             <Image src="/images/trending-1.svg" boxSize={"fit-content"} />
           </GridItem>
-          <GridItem w="100%" h="10">
+          <GridItem w="100%" h="max-content">
             <Image src="/images/trending-2.svg" boxSize={"fit-content"} />
           </GridItem>
-          <GridItem w="100%" h="10">
+          <GridItem w="100%" h="max-content">
             <Image src="/images/trending-3.svg" boxSize={"fit-content"} />
           </GridItem>
-          <GridItem w="100%" h="10">
+          <GridItem w="100%" h="max-content">
             <Image src="/images/trending-1.svg" boxSize={"fit-content"} />
           </GridItem>
         </Grid>
